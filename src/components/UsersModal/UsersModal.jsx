9d30ce0abeb515c6bd66bd7dfd7ec8e809cd548f
Suliman <sssuliman@bk.ru>
2023-02-10 15:08:16 +0300

    adding users in room

--- src/components/UsersModal/UsersModal.jsx
+++ src/components/UsersModal/UsersModal.jsx
@@ -1,22 +1,36 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../../features/applicationSlice";
+import { addUserRoom, fetchRoom } from "../../features/roomSlice";
 import styles from "./usersModal.module.scss";
 
 const UsersModal = () => {
-  const usersAll = useSelector((state) => state.application.users);
+
+  const usersAll = useSelector((state) =>
+    state.application.users.filter(
+      (item) => item._id !== localStorage.getItem("id")
+    )
+  );
   const dispath = useDispatch();
+
+  const handleAdd = (e, user) => {
+    dispath(addUserRoom({ id: localStorage.getItem("room"), user: user._id }));
+  };
 
   useEffect(() => {
     dispath(fetchUsers());
+    dispath(fetchRoom());
   }, [dispath]);
 
   return (
-    <div className={styles.modal}>
-      <div className={styles.close}>x</div>
+    <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
       {usersAll?.map((user) => {
         return (
-          <div key={user._id} className={styles.name}>
+          <div
+            key={user._id}
+            className={styles.name}
+            onClick={(e) => handleAdd(e, user)}
+          >
             {user.name}
           </div>
         );
